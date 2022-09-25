8f2f027b648369d20c7f496dd0eabdf1f0b82217
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-09-24 21:25:01 -0400

    Adicionando estilos

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+//import Script from "next/script";
+import Layout from "../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
+
       <Image
         src="/images/profile.jpeg"
         width={240}
@@ -19,6 +22,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   )
 }
