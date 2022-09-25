3ab42a6f0f6804d5ad5e9e930dab79ea78e96d03
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-09-24 20:53:35 -0400

    Adicionando imagem

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
 
-export default function FirstPost(){
-  return(
+export default function FirstPost() {
+  return (
     <>
+      <Image
+        src="/images/profile.jpeg"
+        width={240}
+        height={240}
+        alt="Carlos Felipe" />
       <h1>
         First Post!
       </h1>
